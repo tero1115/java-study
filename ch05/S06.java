a47793e679e1d54c0bee3fab6a347c432d7d9ea7
tero1115 <limjisang1115@gmail.com>
2023-06-30 16:22:18 +0900

    배열

---
+++ ch05/S06.java
@@ -0,0 +1,22 @@
+package ch05;
+
+public class S06 {
+    public static void main(String[] args) {
+        // 이중 배열
+        // new int[][]를 생략 할 수 있다.
+        int[][] intArray = {
+            {10,20,30},
+            {40,50,60}
+        };
+
+        // int[][] intArray1 = new int[2][3];
+        // intArray[0] = new int[] {10,20,30};
+        // intArray[1] = new int[] {40,50,60};
+
+        for (int i = 0; i < intArray.length; i++) {
+            for (int j = 0; j < intArray[i].length; j++) {
+                System.out.println(intArray[i][j]);
+            }
+        }
+    }
+}
